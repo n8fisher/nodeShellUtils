5126cc937fdbf0bd2e47c00973dddfef119c1993
Fisher <fisher.nathaniel@gmail.com>
2016-03-20 20:29:34 -0700

    Adding comment about non-preservation of whitespace

--- echo.js
+++ echo.js
@@ -5,7 +5,10 @@ let fs = require('fs').promise
 
 
 function* echo() {
-       //console.log(yield fs.readFile(__filename, console.log))
+		//Let's get only the third element to the end of the array
+		//...then print with space seperators
+		//As a note, this has several issue. For example whitespace is not preserved
+		//http://stackoverflow.com/questions/24874084/getting-raw-command-line-arguments-in-node-js
        console.log(process.argv.slice(2,process.argv.length).join(' '))
 }
 

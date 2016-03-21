7a4f8d013d18bba0cda59798d8bcdbb1d71b0183
Fisher <fisher.nathaniel@gmail.com>
2016-03-21 00:00:54 -0700

    Fixing cat

--- cat.js
+++ cat.js
@@ -2,11 +2,22 @@
 
 require('./helper')
 let fs = require('fs').promise
+let co = require('co')
 
-function* cat (){
-		fs.readFile(process.argv[2], (err, contents) => {	
-  			console.log(contents);	
-		});
+let cat = co.wrap( function* () {
+		let contents = yield  fs.readFile(process.argv[2], "UTF8",function (err, contents)  {	
+			if (err){
+				console.log(err)
+			}else {
+				return contents
+			}
+		})
+		console.log(contents)
+})
+
+co = co.wrap(cat)
+function* main(){
+	let noop = yield cat() 
 }
 
-module.exports = cat
+module.exports = main

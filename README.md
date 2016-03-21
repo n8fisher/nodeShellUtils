5e7a0832c64da7d30dba66ffc3c9bc449d829562
Fisher <fisher.nathaniel@gmail.com>
2016-03-20 23:32:07 -0700

    Adding ls

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Time spent: [Write the number of hours you spend here]
 
 Completed:
 
-* [] Required: `echo.js` prints the first argument to stdout
-* [] Required: `cat.js` prints the contents of the first argument to stdout
-* [] Required: `touch.js` updates the modified date of the first argument
+* [x] Required: `echo.js` prints the first argument to stdout
+* [x] Required: `cat.js` prints the contents of the first argument to stdout
+* [x] Required: `touch.js` updates the modified date of the first argument
 * [] Required: `ls.js` recursively lists the files of the first argument
 * [] Required: `mkdir.js` create a directory at the first argument
 * [] Required: `rm.js` deletes any file or directory at the first argument 
---
+++ ls.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+require('./helper')
+
+let _ = require('lodash');
+let fs = require('fs').promise
+let path = require('path')
+let co = require('co')
+let recursive =false
+let dir = process.argv[2]
+
+if (process.argv[2] == '-R'){
+	recursive=true
+	dir=process.argv[3]
+}
+
+
+let ls = co.wrap(function*  (rootPath) {
+ 	let fileNames = yield fs.readdir(rootPath)  
+
+ 	let lsPromises = []
+	for (let fileName of fileNames) {	
+    	let filePath = path.join(rootPath, fileName)  
+    	let stat = yield fs.stat(filePath)
+  		lsPromises.push(filePath)
+  		if(stat.isDirectory()==true && recursive){
+  			let promise = ls(filePath)
+  			lsPromises.push(promise)	
+  		}
+  	}
+  	return yield Promise.all(lsPromises)
+  })
+
+function printDirs(filePaths){
+	for ( let el of _.flattenDeep(filePaths)){
+		console.log(el)
+	}
+}
+
+ls = co.wrap(ls)
+function* main(){
+	let filePaths = yield ls(dir)
+	printDirs(filePaths)
+}
+
+module.exports = main
